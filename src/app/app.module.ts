4a7ffb067ca8c77bb510cab26087e365854204a1
Alejandro Méndez <alejandro.mendez.developer@gmail.com>
2021-12-13 00:23:53 -0600

    Add custom exponential pipe
    Example of implementation, pipe in charge of get the result of square a number

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
+import { ExponentialPipe } from './pipes/exponential.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
+    ExponentialPipe,
     HighlightDirective
   ],
   imports: [
