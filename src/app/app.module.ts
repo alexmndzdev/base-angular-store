b9cc3bcaf52c91ab6e332684c41e481804ecdc57
Alejandro Méndez <alejandro.mendez.developer@gmail.com>
2021-12-13 00:24:57 -0600

    Add new cart component
    Added files and imported in module but still not modified the template component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
 import { ExponentialPipe } from './pipes/exponential.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
 
@@ -11,6 +12,7 @@ import { HighlightDirective } from './directives/highlight.directive';
   declarations: [
     AppComponent,
     ProductComponent,
+    CartComponent,
     ExponentialPipe,
     HighlightDirective
   ],
