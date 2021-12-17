39bdd91a0de95540218101c8e07af21f6ac53bc4
Alejandro Méndez <alejandro.mendez.developer@gmail.com>
2021-12-17 11:33:53 -0600

    Add banner with swiper in banner component
    Modified banner and home components

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-banner',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
+  images: string[] = [
+    'https://dircomfidencial.com/wp-content/uploads/2016/11/logo-1330779_960_720.png.webp',
+    'https://dircomfidencial.com/wp-content/uploads/2016/11/logo-1330779_960_720.png.webp',
+    'https://dircomfidencial.com/wp-content/uploads/2016/11/logo-1330779_960_720.png.webp',
+    //'assets/images/banner-1.jpg',
+    //'assets/images/banner-2.jpg',
+    //'assets/images/banner-3.jpg',
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  mySwiper: Swiper;
+
+  constructor() {
+     this.mySwiper = new Swiper('.swiper-container');
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.mySwiper = new Swiper('.swiper-container');
+  }
 }
